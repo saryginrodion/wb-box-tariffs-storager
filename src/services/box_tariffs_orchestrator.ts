c1c7ca513ef024ed1f535f412f53898a14e83a82
Sarygin Rodion <saryginrodion7@proton.me>
2025-10-02 13:50:23 +0700

    feat: box tariffs orchestrator

---
+++ src/services/box_tariffs_orchestrator.ts
@@ -0,0 +1,26 @@
+import { IBoxTariffsDB } from "#core/interfaces/box_tariffs_db.js";
+import { IBoxTariffsSheet } from "#core/interfaces/box_tariffs_sheet.js";
+import { IWbApi } from "#core/interfaces/wbapi.js";
+import { toWbApiDateFormat } from "#utils/date.js";
+
+export class BoxTariffsOrchestrator {
+    #wbapi: IWbApi
+    #boxTariffsDb: IBoxTariffsDB
+    #boxTariffsSheet: IBoxTariffsSheet
+
+    constructor(wbapi: IWbApi, boxTariffsDb: IBoxTariffsDB, boxTariffsSheet: IBoxTariffsSheet) {
+        this.#wbapi = wbapi;
+        this.#boxTariffsDb = boxTariffsDb;
+        this.#boxTariffsSheet = boxTariffsSheet;
+    }
+
+    async updateBoxTariffs(): Promise<void> {
+        const date = toWbApiDateFormat(new Date());
+
+        const boxTariffs = await this.#wbapi.boxTariffs(date);
+
+        await this.#boxTariffsDb.saveTariffs(date, boxTariffs.warehouseList);
+
+        await this.#boxTariffsSheet.updateSheet(date, boxTariffs.warehouseList);
+    }
+}
