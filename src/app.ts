0059c70404a4e9e38deaf4ca1a38461227f2c6b4
Sarygin Rodion <saryginrodion7@proton.me>
2025-10-03 19:55:05 +0700

    chore: add google creds into dockerfile

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { migrate, seed } from "#postgres/knex.js";
 import { newLogger } from "#utils/logging.js";
 import "#schedule/schedule.js";
-import { updateTariffsBox } from "#schedule/tasks.js";
 
 const logger = newLogger({
     from: "app.ts"
@@ -11,5 +10,3 @@ await migrate.latest();
 await seed.run();
 
 logger.info("All migrations and seeds have been run");
-
-await updateTariffsBox();
