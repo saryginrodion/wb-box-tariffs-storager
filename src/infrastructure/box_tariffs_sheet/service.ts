d5e7f1bf684195b16aa7062e0d8835e3dec1750b
Sarygin Rodion <saryginrodion7@proton.me>
2025-10-03 19:36:47 +0700

    feat: box_tariffs_sheet service

--- src/infrastructure/box_tariffs_sheet/service.ts
+++ src/infrastructure/box_tariffs_sheet/service.ts
@@ -1,5 +1,6 @@
 import { IBoxTariffsSheet } from "#core/interfaces/box_tariffs_sheet.js";
 import { BoxTariff } from "#core/types/box_tariffs.js";
+import { toWbApiDateFormat } from "#utils/date.js";
 import { newLogger } from "#utils/logging.js";
 import { indexesToSpreadsheetRange } from "#utils/spreadsheets.js";
 import { sheets_v4 } from "googleapis";
@@ -19,16 +20,51 @@ export class BoxTariffsSheet implements IBoxTariffsSheet {
     }
 
     async updateSheet(date: Date, tariffs: Array<BoxTariff>): Promise<void> {
-        const rows = [[date,
+        // Headings
+        const rows = [[
+            "Дата",
+            "Имя склада",
+            "Геолокация",
+            "Базовая стоимость доставки",
+            "Коэф. доставки (экспресс)",
+            "Стоимость доставки за литр",
+            "Базовая стоимость доставки (Маркетплейс)",
+            "Коэф. экспресс доставки (Маркетплейс)",
+            "Стоимость доставки за литр (Маркетплейс)",
+            "Базовая стоимость хранения",
+            "Коэф. стоимости хранения (экспресс)",
+            "Стоимость хранения за литр",
         ]];
+
+        await this.writeRows(0, 0, rows);
+        
+        const tariffRows = tariffs.sort((a, b) => {
+            // Сортировка только по коэф. экспресс доставки
+            return (a.boxDeliveryCoefExpr || 0) - (b.boxDeliveryCoefExpr || 0);
+        }).map((tariff, _) => [
+            toWbApiDateFormat(date),
+            tariff.warehouseName,
+            tariff.geoName,
+            tariff.boxDeliveryBase,
+            tariff.boxDeliveryCoefExpr,
+            tariff.boxDeliveryLiter,
+            tariff.boxDeliveryMarketplaceBase,
+            tariff.boxDeliveryMarketplaceCoefExpr,
+            tariff.boxDeliveryMarketplaceLiter,
+            tariff.boxStorageBase,
+            tariff.boxStorageCoefExpr,
+            tariff.boxStorageLiter,
+        ]);
+
+        await this.writeRows(0, 1, tariffRows);
     }
 
-    async writeRows(fromCol: number, fromRow: number, rows: Array<Array<string>>) {
+    async writeRows(fromCol: number, fromRow: number, rows: Array<Array<string | number | null | undefined>>) {
         const rowsWidth = Math.max(...rows.map((row, _) => row.length))
         const colsLength = rows.length;
 
-        const toRow = fromRow + rowsWidth - 1;
-        const toCol = fromCol + colsLength - 1;
+        const toRow = fromRow + colsLength - 1;
+        const toCol = fromCol + rowsWidth - 1;
 
         const range = indexesToSpreadsheetRange(this.#sheetName, fromCol, fromRow, toCol, toRow);
         logger.debug("Writing range " + range);
